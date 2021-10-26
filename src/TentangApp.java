ac6fe8b9955b08038c8b31837fc09afbf3099ef3
Nandang Prayogi <58286951+nandangpy@users.noreply.github.com>
2021-10-27 02:39:39 +0700

    add Fitur

--- src/TentangApp.java
+++ src/TentangApp.java
@@ -40,12 +40,6 @@ public class TentangApp extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    
-    //////////   //////////  //  //
-    //           //      //  ////
-    //           //      //  // \\
-    /////////    //////////  //  \\ 
-    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
